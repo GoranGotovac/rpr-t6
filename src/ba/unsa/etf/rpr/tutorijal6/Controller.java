44a10e8c53131937a12833fe5587f49a64ad713f
GoranGotovac <ggotovac1@etf.unsa.ba>
2018-11-27 18:06:53 +0100

    Ispravljena metoda za validanJMBG

--- src/ba/unsa/etf/rpr/tutorijal6/Controller.java
+++ src/ba/unsa/etf/rpr/tutorijal6/Controller.java
@@ -160,7 +160,7 @@ public class Controller {
 
         if (n.length() < 2 || n.length() > 20) {imeValidno= false; return false; }
         for (int i = 0; i < n.length(); i++) {
-            if (!(n.charAt(i) >= 'A' && n.charAt(i) <= 'Ž') && !(n.charAt(i) >= 'a' && n.charAt(i) <= 'ž')) {
+            if (!((n.charAt(i) >= 'A' && n.charAt(i) <= 'Z') || n.charAt(i) == 'Ž' || n.charAt(i) == 'Č' || n.charAt(i) == 'Ć' || n.charAt(i) == 'Đ' || n.charAt(i) == 'Š') && !(n.charAt(i) >= 'a' && n.charAt(i) <= 'z' || n.charAt(i) == 'ž' || n.charAt(i) == 'č' || n.charAt(i) == 'ć' || n.charAt(i) == 'đ' || n.charAt(i) == 'š')) {
                 imeValidno = false;
                 return false;
             }
@@ -178,8 +178,10 @@ public class Controller {
                 if (!(n.charAt(i) >= '0' && n.charAt(i) <= '9')) {jmbgValidan= false; return false; }
             }
         }
-        jmbgValidan = true;
-        return !n.trim().isEmpty();
+        int L = 11 - ((7*( (n.charAt(0)-'0') + (n.charAt(6)-'0')) + 6*( (n.charAt(1)-'0') + (n.charAt(7)-'0')) + 5*((n.charAt(2)-'0')+ (n.charAt(8)-'0')) + 4*((n.charAt(3)- '0') + (n.charAt(9)-'0')) +3*((n.charAt(4)-'0') + n.charAt(10)-'0') + 2*((n.charAt(5)-'0') + (n.charAt(11)-'0'))) % 11);
+        if((L>=1 && L<=9) && n.charAt(12)-'0' == L) return true;
+        return false;
+
     }
     private boolean validanIndex(String n) {
         if(n.length()!=5 || n.charAt(0) == '0') {indeksValidan = false; return false; }
