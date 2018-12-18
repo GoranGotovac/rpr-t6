283817aa018729d9b4b1fb8cd4508dd89d761b87
GoranGotovac <ggotovac1@etf.unsa.ba>
2018-12-18 17:44:48 +0100

    Popravljen program da se moze kompajlirati

--- Test/ba/unsa/etf/rpr/tutorijal6/MainTest.java
+++ Test/ba/unsa/etf/rpr/tutorijal6/MainTest.java
@@ -7,12 +7,3 @@ import javafx.stage.Stage;
 import org.testfx.api.FxRobot;
 import org.testfx.framework.junit5.ApplicationExtension;
 import org.testfx.framework.junit5.Start;
-@ExtendWith(ApplicationExtension.class)
-class MainTest {
-    @Start
-    public void start (Stage stage) throws Exception {
-        Parent mainNode = FXMLLoader.load(Main.class.getResource("prijava.fxml"));
-        stage.setScene(new Scene(mainNode));
-        stage.show();
-        stage.toFront();
-    }
